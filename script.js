5aed8028377e4c9f5046efdeee9c82de1249d87c
Zk408DevLoper <93755736+Zk408DevLoper@users.noreply.github.com>
2026-09-20 20:15:02 +0800

    Uploaded the files

---
+++ script.js
@@ -0,0 +1,9 @@
+function showsideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+}
+
+function hidesideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+}
